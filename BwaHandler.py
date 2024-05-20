1087363b92eb18d60b9398374fda5c6110e24f83
kimzz14 <kimzz1004@gmail.com>
2024-05-20 17:48:33 +0900

    init

---
+++ BwaHandler.py
@@ -0,0 +1,15 @@
+class BwaHandler:
+    def __init__(self):
+        pass
+
+    def check(self):
+        pass
+
+    def run_index(self):
+        pass
+
+    def run_mem(self):
+        pass
+    
+    def run(self):
+        pass
